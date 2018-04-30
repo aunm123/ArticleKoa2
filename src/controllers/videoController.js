bb4a139b53c450e8bfefe8608adaf69d713266a1
luotim123 <luotim123@yeah.net>
2018-04-30 22:19:47 +0700

    video detail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,30 +2,61 @@ const rq = require('request-promise');
 const {reqUrl} = require('../util/Config.js');
 
 let videoController = {
-	index: async (ctx, next) => {
-		let aid = ctx.params.aid;
-		console.log(aid);
+	detail: async (ctx, next) => {
+		let videoid = ctx.params.videoid;
+		let op = reqUrl('/movice/find', {moviceid: videoid});
+		let video = await rq(op);
+		video = JSON.parse(video);
 
-		// let op = reqUrl('/video/find', {videoid: aid});
-		//
-		// let video = await rq(op);
-		// video = JSON.parse(video);
+		let op2 = reqUrl('/mcategory/find', {mcategoryid: video.mcategoryId});
+		let current_category = await rq(op2);
+		current_category = JSON.parse(current_category);
 
 		await next();
 		await ctx.render('video/video_detail',{
+			video,
+			current_category
+		});
+
+	},
+	list: async (ctx, next) => {
+		// /list/:mcategoryid/:page
+		let mcategoryid = ctx.params.mcategoryid;
+		let page = ctx.params.page;
+
+		let op = reqUrl('/movice/listWithPage', {mcategoryid: mcategoryid, page: page});
+		let videos = await rq(op);
+		videos = JSON.parse(videos);
+
+		let op2 = reqUrl('/mcategory/find', {mcategoryid: mcategoryid});
+		let current_category = await rq(op2);
+		current_category = JSON.parse(current_category);
+
+
+		await next();
+		await ctx.render('video/video_list', {
+			videos,
+			current_category,
+			page,
+			pageCount: Math.ceil(videos.count / 20.0)
 		});
 
 	},
 	play: async (ctx, next) => {
-		let aid = ctx.params.aid;
+		// /play/:videoid
+		let videoid = ctx.params.videoid;
+		let op = reqUrl('/movice/find', {moviceid: videoid});
+		let video = await rq(op);
+		video = JSON.parse(video);
 
-		// let op = reqUrl('/video/find', {videoid: aid});
-		//
-		// let video = await rq(op);
-		// video = JSON.parse(video);
+		let op2 = reqUrl('/mcategory/find', {mcategoryid: video.mcategoryId});
+		let current_category = await rq(op2);
+		current_category = JSON.parse(current_category);
 
 		await next();
 		await ctx.render('video/video_play',{
+			video,
+			current_category
 		});
 
 	}
--- src/routes/video/index.js
+++ src/routes/video/index.js
@@ -1,8 +1,9 @@
 const videoRoute = require('koa-router')();
 const videoController = require('../../controllers/videoController');
 
-videoRoute.get('/:aid', videoController.index);
-videoRoute.get('/play/:aid', videoController.play);
+videoRoute.get('/detail/:videoid', videoController.detail);
+videoRoute.get('/list/:mcategoryid/:page', videoController.list)
+videoRoute.get('/play/:videoid', videoController.play);
 
 
 module.exports = videoRoute;
