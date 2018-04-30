2a0b863457e9ee8a75932361b0790ffa1dd3f09a
luotim123 <luotim123@yeah.net>
2018-04-30 13:12:46 +0700

    video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -16,6 +16,19 @@ let videoController = {
 		});
 
 	},
+	play: async (ctx, next) => {
+		let aid = ctx.params.aid;
+
+		// let op = reqUrl('/video/find', {videoid: aid});
+		//
+		// let video = await rq(op);
+		// video = JSON.parse(video);
+
+		await next();
+		await ctx.render('video/video_play',{
+		});
+
+	}
 }
 
 module.exports = videoController;
--- src/routes/video/index.js
+++ src/routes/video/index.js
@@ -2,6 +2,7 @@ const videoRoute = require('koa-router')();
 const videoController = require('../../controllers/videoController');
 
 videoRoute.get('/:aid', videoController.index);
+videoRoute.get('/play/:aid', videoController.play);
 
 
 module.exports = videoRoute;
